f92a7ac61bde7796b3e92a03135e2cd7f5d4123e
Theocharis Ledakis <git@mail.ledakis.com>
2017-03-29 22:23:00 +0100

    suppresed the error message if ips do not exist yet

--- inv-gen.sh
+++ inv-gen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # generates the inventory file for ansible
 
-sh -c '(echo [nodes]; cat /home/pi/picluster/ip/* )' > /home/pi/picluster/inventory
+sh -c '(echo [nodes]; cat /home/pi/picluster/ip/* ) 2>/dev/null ' > /home/pi/picluster/inventory
