39b0242006bda87ce1e2db85affebf86436d440d
Theocharis Ledakis <git@mail.ledakis.com>
2017-03-29 22:19:14 +0000

    change on reporting the correct mac address in the commit message when commiting the ip

--- up2git.sh
+++ up2git.sh
@@ -21,7 +21,7 @@ ip_changed=$(./ip2file.sh)
 
 if [ $ip_changed ]; then
 	git -C /home/pi/picluster/ add /home/pi/picluster/ip/$(cat /sys/class/net/eth0/address | tr -d ":")
-	git -C /home/pi/picluster/ commit -m 'new ip for $(cat /sys/class/net/eth0/address | tr -d ":")'
+	git -C /home/pi/picluster/ commit -m "new ip for $(cat /sys/class/net/eth0/address | tr -d ':')"
 	git -C /home/pi/picluster/ push origin master
 fi
 
