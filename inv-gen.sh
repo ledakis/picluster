9a8da9c3cadd066a61d5a83b0bc0d3bd60bcb720
Theocharis Ledakis <git@mail.ledakis.com>
2017-03-30 07:35:33 +0100

    modified inv-gen, for the openmpi inventory file too

--- inv-gen.sh
+++ inv-gen.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # generates the inventory file for ansible
 
-sh -c '(echo [nodes]; cat /home/pi/picluster/ip/* ) 2>/dev/null ' > /home/pi/picluster/inventory
+#sh -c '(echo [nodes]; cat /home/pi/picluster/ip/* ) 2>/dev/null ' > /home/pi/picluster/inventory
+sh -c 'cat /home/pi/picluster/ip/* 2>/dev/null ' > /home/pi/picluster/inventory
