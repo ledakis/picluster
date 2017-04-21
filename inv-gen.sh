cb065f97e45ba7f836ede531726cbd4fc8b8c41c
Theocharis Ledakis <git@mail.ledakis.com>
2017-04-21 12:48:47 +0300

    changes to inventory creation script

--- inv-gen.sh
+++ inv-gen.sh
@@ -2,4 +2,13 @@
 # generates the inventory file for ansible
 
 #sh -c '(echo [nodes]; cat /home/pi/picluster/ip/* ) 2>/dev/null ' > /home/pi/picluster/inventory
-sh -c 'cat /home/pi/picluster/ip/* 2>/dev/null ' > /home/pi/picluster/inventory
+
+#sh -c 'cat /home/pi/picluster/ip/* 2>/dev/null ' > /home/pi/picluster/inventory
+echo "[main]" > inventory
+cat ip/* 2>/dev/null >> inventory
+
+echo "
+[main:vars]
+ansible_user=root" >> inventory
+
+
