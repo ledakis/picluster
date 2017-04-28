70c8557bd7614911891907b16ac0f99f2eff5e9d
Theocharis Ledakis <git@mail.ledakis.com>
2017-04-29 00:29:39 +0100

    added the new playbooks

--- systemfiles/masterRun.sh
+++ systemfiles/masterRun.sh
@@ -11,5 +11,7 @@ pgrep ansible > /dev/null
 sts=$?
 
 if [ $sts -ne 0 ]; then
-        ansible-playbook -i $PI_local_repo_dir/inventory $PI_local_repo_dir/play.yml > /home/pi/ansible.log
+        ansible-playbook -i $PI_local_repo_dir/inventory $PI_local_repo_dir/master.yml > /home/pi/ansible.log
+        ansible-playbook -i $PI_local_repo_dir/inventory $PI_local_repo_dir/nodes.yml >> /home/pi/ansible.log
+
 fi
